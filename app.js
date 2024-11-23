70f02a431c8a5f6a16dc3e3af0f7e1b8ecbecaf9
Willmar R. de Ramos <willrodrigue2308s@gmail.com>
2024-11-23 20:02:13 -0300

    limite alterado para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 console.log(`O número é ${numeroSecreto}`);
 let tentativas = 1;
@@ -19,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
@@ -41,7 +41,7 @@ function verificarChute() {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
         tentativas++;
-        limparCampo()
+        limparCampo();
     }
 }
 
@@ -57,7 +57,7 @@ function gerarNumeroAleatorio() {
         return gerarNumeroAleatorio();
     } else {
         listaDeNumerosSorteados.push(numeroEscolhido);
-        console.log(listaDeNumerosSorteados)
+        console.log(listaDeNumerosSorteados);
         return numeroEscolhido;
     }
 }
